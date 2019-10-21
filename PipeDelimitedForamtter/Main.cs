158d8e243738dd69cc36e012bac2cac2d6b1e49b
Logan <meissnerl@alumni.msoe.edu>
2019-10-20 21:08:55 -0500

    Initial Commit

---
+++ PipeDelimitedForamtter/Main.cs
@@ -0,0 +1,71 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Kbg.NppPluginNET.PluginInfrastructure;
+
+namespace Kbg.NppPluginNET
+{
+    class Main
+    {
+        internal const string PluginName = "PipeDelimitedFormatter";
+        static string iniFilePath = null;
+        static bool someSetting = false;
+        static frmMyDlg frmMyDlg = null;
+        static int idMyDlg = -1;
+        static Bitmap tbBmp = Properties.Resources.star;
+        static Bitmap tbBmp_tbTab = Properties.Resources.star_bmp;
+        static Icon tbIcon = null;
+        static IScintillaGateway editor = new ScintillaGateway(PluginBase.GetCurrentScintilla());
+
+        public static void OnNotification(ScNotification notification)
+        {  
+            // This method is invoked whenever something is happening in notepad++
+            // use eg. as
+            // if (notification.Header.Code == (uint)NppMsg.NPPN_xxx)
+            // { ... }
+            // or
+            //
+            // if (notification.Header.Code == (uint)SciMsg.SCNxxx)
+            // { ... }
+        }
+
+        internal static void CommandMenuInit()
+        {
+            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
+            iniFilePath = sbIniFilePath.ToString();
+            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
+            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
+            someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
+
+            PluginBase.SetCommand(0, "FormatText", FormatText, new ShortcutKey(true, true, false, Keys.P));
+        }
+
+        internal static void PluginCleanUp()
+        {
+            Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
+        }
+
+
+        internal static void FormatText()
+        {
+            var textLength = editor.GetTextLength();
+
+            var documentText = editor.GetText(textLength);
+
+            var splitText = documentText.Split('|');
+
+            var sb = new StringBuilder();
+
+            foreach (string text in splitText)
+            {
+                sb.AppendLine(text);
+            }
+
+            editor.ClearAll();
+
+            editor.SetText(sb.ToString());
+        }
+    }
+}
